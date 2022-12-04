60eb4f77197c187a08708f5b1f2c4327a701cf46
Aaron Graddy Jr <atgraddyjr@gmail.com>
2022-12-03 19:35:19 -0600

    Took out 1 game simulation to put it in a function.

--- main.py
+++ main.py
@@ -2,12 +2,49 @@ import random
 import statistics
 
 dice = [0, 0, 0, 1, 2, 3]
-
 # Keep = 0, left = 1, right = 2, center = 3
+
 
 def roll_dice():
     output = random.randint(0, 5)
     return dice[output]
+
+
+def play_one_game(no_players):
+    players_cash = [3 for i in range(no_players)]
+    center_cash = 0
+    current_player = -1
+    all_rolls = 0
+    stop = False
+    while not stop:  # 1 Game Running
+        if current_player == no_players:  # Setting current player in players_cash list
+            current_player = 0
+        else:
+            current_player += 1
+        current_player = current_player % no_players
+
+        if players_cash[current_player] < 4:  # Setting amount of rolls the current player has
+            no_rolls = players_cash[current_player]
+        else:
+            no_rolls = 3
+
+        all_rolls += 1
+        for j in range(no_rolls):  # Rolling no_rolls times
+            roll_result = roll_dice()
+            if roll_result == 1:  # Rolls Left
+                players_cash[current_player] -= 1
+                players_cash[current_player - 1] += 1
+            if roll_result == 2:  # Rolls Right
+                players_cash[current_player] -= 1
+                players_cash[(current_player + 1) % no_players] += 1
+            if roll_result == 3:  # Rolls Center
+                players_cash[current_player] -= 1
+                center_cash += 1
+                if center_cash == (no_players * 3) - 1 or center_cash >= no_players * 3:  # Stops game
+                    return all_rolls
+            if roll_result == 0:  # Rolls Keep
+                if center_cash == (no_players * 3) - 1 or center_cash > no_players * 3:  # Stops game
+                    return all_rolls
 
 
 def simulate(no_players, no_games, no_seconds):
@@ -16,49 +53,14 @@ def simulate(no_players, no_games, no_seconds):
     all_times = []
     completed_games = 0
     for i in range(no_games):  # Plays number of games
-        players_cash = [3 for i in range(no_players)]
-        center_cash = 0
-        current_player = -1
-        all_rolls = 0
-        stop = False
-        while not stop:  # 1 Game Running
-            if current_player == no_players:  # Setting current player in players_cash list
-                current_player = 0
-            else:
-                current_player += 1
-            current_player = current_player % no_players
-
-            if players_cash[current_player] < 4:  # Setting amount of rolls the current player has
-                no_rolls = players_cash[current_player]
-            else:
-                no_rolls = 3
-
-            all_rolls += 1
-            for j in range(no_rolls):  # Rolling no_rolls times
-                roll_result = roll_dice()
-                if roll_result == 1:  # Rolls Left
-                    players_cash[current_player] -= 1
-                    players_cash[current_player - 1] += 1
-                if roll_result == 2:  # Rolls Right
-                    players_cash[current_player] -= 1
-                    players_cash[(current_player + 1) % no_players] += 1
-                if roll_result == 3:  # Rolls Center
-                    players_cash[current_player] -= 1
-                    center_cash += 1
-                    if center_cash == (no_players * 3) - 1 or center_cash >= no_players * 3:  # Stops game
-                        all_games.append(all_rolls)
-                        stop = True
-                if roll_result == 0:  # Rolls Keep
-                    if center_cash == (no_players * 3) - 1 or center_cash > no_players * 3:  # Stops game
-                        all_games.append(all_rolls)
-                        stop = True
-        all_times.append(no_seconds * all_rolls)
+        all_rolls = play_one_game(no_players)
+        all_games.append(all_rolls)
+        all_times.append(no_seconds*all_rolls)
         completed_games += 1
         percentage_left = round((completed_games / no_games) * 100, 1)  # Percent left in simulation
         if percentage_left >= next_target_percentage and not next_target_percentage > 100:  # Decides when to print %
             print(str(percentage_left) + "%")
             next_target_percentage += 1
-
     sec = round(statistics.mean(all_times))  # Average Seconds Per Game
     time = ""
     for d, u in [(60, "second"), (60, "minute"), (24, "hour"), (sec, "day")]:
@@ -72,5 +74,4 @@ def simulate(no_players, no_games, no_seconds):
         time)
 
 
-print(simulate(8, 100000, 10))  # Amount Of players, Amount of Games, Seconds Per Roll
-
+print(simulate(8, 1000, 10))  # Amount Of players, Amount of Games, Seconds Per Roll
